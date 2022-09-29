69f1ddaefd6a186093929bbe25a8d72d74e4ec62
IslamAhmed1092 <eslam1092@hotmail.com>
2022-09-29 13:38:28 +0200

    add service layer to the customer

--- src/main/java/com/example/webcustomertracker/controller/CustomerController.java
+++ src/main/java/com/example/webcustomertracker/controller/CustomerController.java
@@ -2,9 +2,11 @@ package com.example.webcustomertracker.controller;
 
 import com.example.webcustomertracker.dao.CustomerDAO;
 import com.example.webcustomertracker.entity.Customer;
+import com.example.webcustomertracker.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,12 +16,12 @@ import java.util.List;
 public class CustomerController {
 
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model model) {
 
-        List<Customer> customers = customerDAO.getCustomers();
+        List<Customer> customers = customerService.getCustomers();
 
         model.addAttribute("customers", customers);
 
---
+++ src/main/java/com/example/webcustomertracker/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.example.webcustomertracker.service;
+
+import com.example.webcustomertracker.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+
+}
